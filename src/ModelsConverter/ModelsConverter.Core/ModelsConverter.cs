44a3a8d9bc1cf7c0a9fbabe05cf3210cf618fcf3
Marcin Bigos <marcin.bigos99@gmail.com>
2022-01-25 17:40:04 +0100

    init

---
+++ src/ModelsConverter/ModelsConverter.Core/ModelsConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsConverter.Core
+{
+    public class ModelsConverter
+    {
+        private readonly Type[] types;
+
+        internal ModelsConverter(Type[] types)
+        {
+            this.types = types;
+        }
+
+        public static ModelsConverter Load(Type[] types)
+        {
+            return new ModelsConverter(types);
+        }
+
+        public ModelsConverter ConvertTo(SupportedLanguages language)
+        {
+            return this;
+        }
+
+        public string Read()
+        {
+            return string.Empty;
+        }
+
+        public ModelsConverter SaveTo(string path)
+        {
+            return this;
+        }
+    }
+
+    public enum SupportedLanguages
+    {
+        TypeScript = 1
+    }
+}
