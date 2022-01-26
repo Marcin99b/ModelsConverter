b69c481f5c7a3d9d43e4a198d5a897ccc8d124de
Marcin Bigos <marcin.bigos99@gmail.com>
2022-01-26 17:04:25 +0100

    more basic types

--- src/ModelsConverter/ModelsConverter.Core.Tests/TestModels/BasicTypes.cs
+++ src/ModelsConverter/ModelsConverter.Core.Tests/TestModels/BasicTypes.cs
@@ -15,9 +15,20 @@ namespace ModelsConverter.Core.Tests.TestModels
     {
         public long A { get; set; }
     }
+
+    public class UlongClass
+    {
+        public ulong A { get; set; }
+    }
+
     public class IntClass
     {
         public int A { get; set; }
+    }
+
+    public class UintClass
+    {
+        public uint A { get; set; }
     }
 
     public class ShortClass
@@ -25,9 +36,29 @@ namespace ModelsConverter.Core.Tests.TestModels
         public short A { get; set; }
     }
 
+    public class UshortClass
+    {
+        public ushort A { get; set; }
+    }
+
+    public class ByteClass
+    {
+        public byte A { get; set; }
+    }
+
     public class FloatClass
     {
         public float A { get; set; }
+    }
+
+    public class DoubleClass
+    {
+        public double A { get; set; }
+    }
+
+    public class DecimalClass
+    {
+        public decimal A { get; set; }
     }
 
     public class BoolClass
